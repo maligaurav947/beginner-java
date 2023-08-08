eb57377aa451d673930960ebbb33de8d3e123d07
Mali Gaurav <maligaurav947@gmail.com>
2023-08-08 11:58:55 +0530

    Create Sub.java

---
+++ Sub.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+class Sub
+{  
+  public static void main(String[] args)
+   {
+    int a=10;
+    int b=10;
+    int temp = a + b;
+    System.out.println("a:" + " " + a + " " + "b:" + " " + b);
+    System.out.println("Total Number:" + " " +temp);
+ }
+}
