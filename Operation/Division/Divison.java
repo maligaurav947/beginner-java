70dca27a91c1d455d0c8bc609c32319a2eaf1c65
Mali Gaurav <maligaurav947@gmail.com>
2023-08-25 11:20:00 +0530

    Operation

---
+++ Operation/Division/Divison.java
@@ -0,0 +1,13 @@
+//Divison on Two Sumber
+
+import java.lang.*;
+
+class Divison {
+ public static void main(String[] args) {
+  int a = 10;
+  int b = 10;
+  int temp = a / b;
+  System.out.println("a:" + " " + a + " " + "b:" + " " + b);
+  System.out.println("Total Number:" + " " + temp);
+ }
+}
---
+++ Operation/Subraction/Subraction.java
@@ -0,0 +1,13 @@
+//Subraction on Two Sumber
+
+import java.lang.*;
+
+class Subraction {
+ public static void main(String[] args) {
+  int a = 10;
+  int b = 10;
+  int temp = a - b;
+  System.out.println("a:" + " " + a + " " + "b:" + " " + b);
+  System.out.println("Total Number:" + " " + temp);
+ }
+}
